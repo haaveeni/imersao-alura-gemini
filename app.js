8a924f6c3d11ebff29cd31cd3c7351c15e2d2628
Veridiana Lucena <veridianalucena@gmail.com>
2024-09-10 10:48:28 -0300

    aula 3

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Matrix");
+console.log(dados);
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+  {
+    titulo: "Matrix",
+    descricao: "Matrix é um clássico da ficção científica que nos leva a questionar a natureza da realidade. A história se passa em um futuro distópico onde a humanidade é aprisionada em uma simulação virtual, a Matrix, criada por máquinas inteligentes. Um hacker chamado Neo descobre a verdade e se junta a uma rebelião contra as máquinas, buscando libertar a humanidade. O filme explora temas como livre arbítrio, identidade e a busca pela verdade, e se tornou um marco na cultura pop por suas cenas de ação inovadoras e sua profunda reflexão filosófica.",
+    link: "https://pt.wikipedia.org/wiki/Matrix",
+  },
+  {
+    titulo: "Her",
+    descricao: "Her é um filme de ficção científica que explora a relação entre humanos e inteligências artificiais. Theodore Twombly, um escritor solitário, desenvolve um relacionamento emocional com Samantha, um sistema operacional com inteligência artificial. O filme levanta questões sobre amor, solidão e a natureza da consciência.",
+    link: "https://pt.wikipedia.org/wiki/Her_(filme)"
+  },
+  {
+    titulo: "The Social Network",
+    descricao: "The Social Network é um drama biográfico que narra a história da criação do Facebook. O filme retrata a trajetória de Mark Zuckerberg e as disputas legais que envolveram a criação da maior rede social do mundo.",
+    link: "https://pt.wikipedia.org/wiki.A_Rede_Social"
+  }
+]
